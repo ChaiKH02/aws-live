89b13785956a9bb9f3d2bf14b3feb49be55fbb68
HUN <118953631+ChaiKH02@users.noreply.github.com>
2023-12-19 11:44:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.czb4k0tuypay.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chaikianhun-employee"
+customregion = "us-east-1"
+
